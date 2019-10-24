c902f505490b3032f118ba5eada2948d006eaeb5
camilo contreras <camilo.coo250@gmail.com>
2019-10-24 12:40:44 -0500

    directory component success

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,42 +1,12 @@
 import React from 'react'
 import './homepage.styles.scss'
+import Directory from '../../components/directory/directory.component'
 
 const HomePage = () => {
     return (
         <React.Fragment>
             <div className="homepage">
-                <div className="directory-menu">
-                    <div className="menu-item">
-                        <div className="content">
-                            <h1 className="title">HATS</h1>
-                            <span className="subtitle">Shop</span>
-                        </div>
-                    </div>
-                    <div className="menu-item">
-                        <div className="content">
-                            <h1 className="title">Jackets</h1>
-                            <span className="subtitle">Shop</span>
-                        </div>
-                    </div>
-                    <div className="menu-item">
-                        <div className="content">
-                            <h1 className="title">Sneakers</h1>
-                            <span className="subtitle">Shop</span>
-                        </div>
-                    </div>
-                    <div className="menu-item">
-                        <div className="content">
-                            <h1 className="title">Womens</h1>
-                            <span className="subtitle">Shop</span>
-                        </div>
-                    </div>
-                    <div className="menu-item">
-                        <div className="content">
-                            <h1 className="title">Mens</h1>
-                            <span className="subtitle">Shop</span>
-                        </div>
-                    </div>
-                </div>
+            <Directory/>
             </div>
         </React.Fragment>
     )
