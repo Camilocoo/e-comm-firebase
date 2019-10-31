0fe62d785f922ab3d5ac6dd58cea68deec764348
camilo contreras <camilo.coo250@gmail.com>
2019-10-31 14:07:17 -0500

    user redirected to homepage when he logs in

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react'
 import HomePage from './pages/homepage/homepage.component'
 import shopPage from './pages/shop/shop.component'
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route,Redirect} from 'react-router-dom'
 import './App.css';
 import Header from '../src/components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-out/sign-in-and-out.component'
@@ -49,17 +49,25 @@ class App extends Component {
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage}/>
-          <Route exact path="/signin" component={SignInAndSignUpPage}/>
-          <Route exact patht="/shop" component={shopPage}/>
+          <Route exact path="/signin" render={()=>this.props.currentUser?(<Redirect to='/'/>):(<SignInAndSignUpPage/>)}/>
+          <Route  path="/shop" component={shopPage}/>
         </Switch>
       </div>
     )
   }
 }
 
+const mapStateToProps =({user})=>({
+ currentUser:user.currentUser
+})
+
+
 const mapDispatchToProps=dispatch=>({
 setCurrentUser:user=>dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps  
+  )(App)
 
